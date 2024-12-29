e3ac92df9c2598d70ebff1468c6465917e964e42
Timofey <tima.khlystov99@mail.ru>
2024-12-29 17:16:07 +0300

    fix linters

--- lms/urls.py
+++ lms/urls.py
@@ -1,24 +1,22 @@
 from django.urls import path
-
-from lms.apps import LmsConfig
 from rest_framework.routers import DefaultRouter
 
-from lms.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetrieveAPIView, LessonUpdateAPIView, \
-    LessonDestroyAPIView, PaymentListAPIView, PaymentCreateAPIView
+from lms.apps import LmsConfig
+from lms.views import (CourseViewSet, LessonCreateAPIView, LessonDestroyAPIView, LessonListAPIView,
+                       LessonRetrieveAPIView, LessonUpdateAPIView, PaymentCreateAPIView, PaymentListAPIView,)
+
 
 app_name = LmsConfig.name
 
 router = DefaultRouter()
-router.register(r'course', CourseViewSet, basename='course')
+router.register(r"course", CourseViewSet, basename="course")
 
 urlpatterns = [
-    path('lesson/create/', LessonCreateAPIView.as_view(), name='lesson-create'),
-    path('lesson/list/', LessonListAPIView.as_view(), name='lesson-list'),
-    path('lesson/<int:pk>/', LessonRetrieveAPIView.as_view(), name='lesson-retrieve'),
-    path('lesson/update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson-update'),
-    path('lesson/destroy/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson-destroy'),
-
-
-    path('payment/create/', PaymentCreateAPIView.as_view(), name='payment-create'),
-    path('payment/list/', PaymentListAPIView.as_view(), name='payment-list'),
+    path("lesson/create/", LessonCreateAPIView.as_view(), name="lesson-create"),
+    path("lesson/list/", LessonListAPIView.as_view(), name="lesson-list"),
+    path("lesson/<int:pk>/", LessonRetrieveAPIView.as_view(), name="lesson-retrieve"),
+    path("lesson/update/<int:pk>/", LessonUpdateAPIView.as_view(), name="lesson-update"),
+    path("lesson/destroy/<int:pk>/", LessonDestroyAPIView.as_view(), name="lesson-destroy"),
+    path("payment/create/", PaymentCreateAPIView.as_view(), name="payment-create"),
+    path("payment/list/", PaymentListAPIView.as_view(), name="payment-list"),
 ] + router.urls
