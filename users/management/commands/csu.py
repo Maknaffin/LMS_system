223f61dfc4eb183cbd5425af6e8c4906db32d8b1
Timofey <tima.khlystov99@mail.ru>
2024-12-29 17:19:55 +0300

    fix linters

--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -7,12 +7,12 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         user = User.objects.create(
-            email='admin@mail.ru',
-            first_name='Admin',
-            last_name='Admin',
+            email="admin@mail.ru",
+            first_name="Admin",
+            last_name="Admin",
             is_staff=True,
             is_superuser=True,
         )
 
-        user.set_password('Qwerty1234')
+        user.set_password("Qwerty1234")
         user.save()
